612c854e6d512e87c15b90f2bb1056baab078107
Alexander Rymdeko-Harvey <iamfree2009@gmail.com>
2016-09-07 10:27:40 -0400

    Git Updates

---
+++ NtLoadDriver/EXE/NtLoadDriver-C++/stdafx.h
@@ -0,0 +1,19 @@
+#pragma once
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+//#include <shlwapi.h>
+#include <stdio.h>
+//#pragma comment(lib, "shlwapi.lib")
+
+// TODO: reference additional headers your program requires here
+
